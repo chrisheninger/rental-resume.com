bac5fa61a3509aa8a96b328030ca4921f1b8ae64
Ryan Wiemer <ryan@ryanwiemer.com>
2017-01-31 15:53:44 -0800

    Making all sections optional

--- app/components/Preview.js
+++ app/components/Preview.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { numberWithCommas } from '../util/helpers';
+// import { DragDropContext } from 'react-dnd';
+// import HTML5Backend from 'react-dnd-html5-backend';
 
 class Preview extends React.Component {
   constructor() {
@@ -17,31 +19,27 @@ class Preview extends React.Component {
       return null;
     }
     if (!applicant.name || !applicant.email || !applicant.phone) {
-      return (
-        <li className="applicant incomplete" key={index}>
-          <Link to="/applicant">This section is incomplete</Link>
-        </li>
-      );
+      console.log ( 'Applicant info is missing...' );
+      return null;
     }
     return (
-      <li className="applicant" key={index}>
-        <div className="applicant__name">{applicant.name}</div>
-        <div className="applicant__email">{applicant.email}</div>
-        <div className="applicant__phone">{applicant.phone}</div>
-      </li>
+      <section className="resume__section resume__section--applicant">
+        <h2 className="resume__subtitle">Applicant</h2>
+        <ul className="applicant-list">
+          <li className="applicant" key={index}>
+            <div className="applicant__name">{applicant.name}</div>
+            <div className="applicant__email">{applicant.email}</div>
+            <div className="applicant__phone">{applicant.phone}</div>
+          </li>
+        </ul>
+      </section>
     );
   }
 
   renderIncomeSection(income) {
     if (!income) {
-      return (
-        <section className="resume__section resume__section--income">
-          <h2 className="resume__subtitle">Income</h2>
-          <p className="applicant-income incomplete">
-            <Link to="/income">This section is incomplete</Link>
-          </p>
-        </section>
-      );
+      console.log ( 'Income info is missing...' );
+      return null;
     }
     return (
       <section className="resume__section resume__section--income">
@@ -55,14 +53,8 @@ class Preview extends React.Component {
 
   renderSummarySection(summary) {
     if (!summary) {
-      return (
-        <section className="resume__section resume__section--summary">
-          <h2 className="resume__subtitle">Summary</h2>
-          <p className="applicant-summary incomplete">
-            <Link to="/summary">This section is incomplete</Link>
-          </p>
-        </section>
-      );
+      console.log ( 'Summary info is missing...' );
+      return null;
     }
     return (
       <section className="resume__section resume__section--summary">
@@ -79,21 +71,23 @@ class Preview extends React.Component {
       return null;
     }
     if (!employment.title || !employment.company || !employment.dateStart || !employment.dateEnd) {
-      return (
-        <li className="job incomplete" key={index}>
-          <Link to="/employment-history">This section is incomplete</Link>
-        </li>
-      );
+      console.log ( 'Employment History info is missing...' );
+      return null;
     }
     return (
-      <li className="job" key={index}>
-        {
-          `${employment.title} at ${employment.company}`
-        }
-        {
-          ` (${employment.dateStart} - ${employment.dateEnd})`
-        }
-      </li>
+      <section className="resume__section resume__section--employment">
+        <h2 className="resume__subtitle">Employment History</h2>
+        <ul className="employment-list">
+          <li className="job" key={index}>
+            {
+              `${employment.title} at ${employment.company}`
+            }
+            {
+              ` (${employment.dateStart} - ${employment.dateEnd})`
+            }
+          </li>
+        </ul>
+      </section>
     );
   }
 
@@ -102,24 +96,26 @@ class Preview extends React.Component {
       return null;
     }
     if (!rental.address1 || !rental.city || !rental.state || !rental.zip || !rental.dateStart || !rental.dateEnd || !rental.reason) {
-      return (
-        <li className="apartment incomplete" key={index}>
-          <Link to="/rental-history">This section is incomplete</Link>
-        </li>
-      );
+      console.log ( 'Rental History info is missing...' );
+      return null;
     }
     return (
-      <li className="apartment" key={index}>
-        {
-          `${rental.address1} ${rental.address2} ${rental.city}, ${rental.state} ${rental.zip}`
-        }
-        <br />
-        {
-          ` (${rental.dateStart} - ${rental.dateEnd})`
-        }
-        <br />
-        <em>Reason For Leaving:</em> {rental.reason}
-      </li>
+      <section className="resume__section resume__section--rent">
+        <h2 className="resume__subtitle">Rental History</h2>
+        <ul className="rental-list">
+          <li className="apartment" key={index}>
+            {
+              `${rental.address1} ${rental.address2} ${rental.city}, ${rental.state} ${rental.zip}`
+            }
+            <br />
+            {
+              ` (${rental.dateStart} - ${rental.dateEnd})`
+            }
+            <br />
+            <em>Reason For Leaving:</em> {rental.reason}
+          </li>
+        </ul>
+      </section>
     );
   }
 
@@ -135,26 +131,38 @@ class Preview extends React.Component {
               </button>
               <h1 className="resume__title">Rental Resume</h1>
             </header>
-            <section className="resume__section resume__section--applicant">
-              <h2 className="resume__subtitle">Applicant</h2>
-              <ul className="applicant-list">
-                {this.props.people.map(this.renderHeader)}
-              </ul>
-            </section>
+
+            {this.props.people.map(this.renderHeader)}
+
             {this.renderIncomeSection(this.props.income)}
+
             {this.renderSummarySection(this.props.summary)}
-            <section className="resume__section resume__section--employment">
-              <h2 className="resume__subtitle">Employment History</h2>
-              <ul className="employment-list">
-                {this.props.employmentHistory.map(this.renderEmploymentHistory)}
+
+            {this.props.employmentHistory.map(this.renderEmploymentHistory)}
+
+            {this.props.rentalHistory.map(this.renderRentalHistory)}
+
+            <section className="resume__section incomplete">
+              <h2 className="resume__subtitle incomplete__subtitle incomplete__sbutitle--active">Omitted Sections</h2>
+              <ul className="incomplete-list">
+                <li className="incomplete__link incomplete__link--applicant">
+                  <Link to="/applicant">Applicant</Link>
+                </li>
+                <li className="incomplete__link incomplete__link--summary">
+                  <Link to="/summary">Summary</Link>
+                </li>
+                <li className="incomplete__link incomplete__link--employment-history">
+                  <Link to="/employment-history">Employment History</Link>
+                </li>
+                <li className="incomplete__link incomplete__link--rental-history">
+                  <Link to="/rental-history">Rental History</Link>
+                </li>
+                <li className="incomplete__link incomplete__link--income">
+                  <Link to="/income">Income</Link>
+                </li>
               </ul>
             </section>
-            <section className="resume__section resume__section--rent">
-              <h2 className="resume__subtitle">Rental History</h2>
-              <ul className="rental-list">
-                {this.props.rentalHistory.map(this.renderRentalHistory)}
-              </ul>
-            </section>
+
           </div>
         </div>
       </section>
