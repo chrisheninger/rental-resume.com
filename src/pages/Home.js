a6b1b5e5ff66200ec43d127b0b51e1225d410236
Chris Heninger <chris@igravitystudios.com>
2017-12-07 22:40:56 -0700

    Home to JSS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,74 @@
 import React from 'react';
+import withStyles from 'react-jss';
 import { Link } from 'react-router-dom';
 
-function Home() {
+import backgroundImage from '../images/homepage.jpg';
+
+function Home({ classes }) {
   return (
-    <main>
-      <section className="home">
-        <div className="home__text">
-          <h2 className="home__text__title">
-            From SF to NY create a rental resume to show your future landlord
-            what sets you apart.
-          </h2>
-          <Link to="/about" className="btn btn--learn-more">
-            Learn More
-          </Link>
-          <Link to="/applicant" className="btn btn--get-started">
-            Get Started
-          </Link>
-        </div>
+    <main className={classes.root}>
+      <section className={classes.container}>
+        <h2 className={classes.headline}>
+          From SF to NY create a rental resume to show your future landlord what
+          sets you apart.
+        </h2>
+        <Link to="/about" className="btn btn--learn-more">
+          Learn More
+        </Link>
+        <Link to="/applicant" className="btn btn--get-started">
+          Get Started
+        </Link>
       </section>
     </main>
   );
 }
-export default Home;
+export default withStyles(theme => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    minHeight: '100vh',
+
+    '&:before': {
+      content: '""',
+      height: '100%',
+      width: '100%',
+      position: 'fixed',
+      top: '0',
+      left: '0',
+      right: '0',
+      bottom: '0',
+      backgroundImage: `url(${backgroundImage})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      zIndex: '-2',
+    },
+
+    '&:after': {
+      content: '""',
+      height: '100%',
+      width: '100%',
+      position: 'fixed',
+      top: '0',
+      left: '0',
+      right: '0',
+      bottom: '0',
+      backgroundColor: '#334050',
+      opacity: '.9',
+      zIndex: '-1',
+    },
+  },
+  container: {
+    maxWidth: '640px',
+    padding: '0 1em',
+    margin: '0 auto',
+  },
+  headline: {
+    // fontSize: '2.25em', // todo: media query for medium+
+    fontSize: '1.75em',
+    lineHeight: '1.15',
+    color: 'white',
+    margin: '0 0 1em 0',
+  },
+}))(Home);
